df3da90737d9cd77743f3e12ebc6da7cbd64cb06
jerry <lwj0173@gmail.com>
2024-07-12 22:43:50 +0900

    First Commit

---
+++ android/app/src/main/kotlin/com/example/vdo_player/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.vdo_player
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:vdo_player/screen/home_screen.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+        home:HomeScreen(),
+      ));
+}
